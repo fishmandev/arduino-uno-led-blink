89f4bce78a2c702dd3b9dfe5ce175371d826a9b2
דמיטרי פישמן <fishman.code@gmail.com>
2019-02-27 21:36:27 +0200

    Add led blink implementation

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(void)
 {
+    DDRB = 0x20;  // Set up PB5 as output
+
     while(1)
     {
-        // TODO: Not implemented yet
+        PORTB |=(1<<PIN5);
+        _delay_ms(1000);
+
+        PORTB &= ~(1<<PIN5);
+        _delay_ms(1000);
     }
     return 0;
 }
